4d2324a350270af7b9519d04c5aec009b84d5569
harveyb <harveyb@myAdminVM.j4zovsaaaarudhe3vaeufwngzd.xx.internal.cloudapp.net>
2017-07-08 22:38:44 +0000

    adding templates

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -10,9 +10,11 @@ end
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
+  notifies :restart, 'service[httpd]', :immediately
 end
 
 service 'httpd' do
  action [:enable, :start]
+#subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
 
